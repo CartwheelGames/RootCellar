16abd16d6ff767361676a7d1298e041cb288d81a
Michael Consoli <michael@cartwheelgames.com>
2023-02-04 13:48:44 -0500

    App state manager

--- Assets/Scripts/AppStateManager.cs
+++ Assets/Scripts/AppStateManager.cs
@@ -1,6 +1,55 @@
-﻿namespace AssemblyCSharp.Assets.Scripts
+﻿using AssemblyCSharp.Assets.Data;
+using System;
+using System.Collections.Generic;
+
+namespace AssemblyCSharp.Assets.Scripts
 {
 	public sealed class AppStateManager
 	{
+		public Dictionary<AppState, List<Action>> enterCallbacks = new();
+
+		public Dictionary<AppState, List<Action>> leaveCallbacks = new();
+
+		public AppState CurrentAppState { get; private set; }
+
+		public void AddEnterListener(AppState appState, Action callback)
+		{
+			if (!enterCallbacks.ContainsKey(appState))
+			{
+				enterCallbacks.Add(appState, new List<Action>());
+			}
+			enterCallbacks[appState].Add(callback);
+		}
+
+		public void AddLeaveListener(AppState appState, Action callback)
+		{
+			if (!leaveCallbacks.ContainsKey(appState))
+			{
+				leaveCallbacks.Add(appState, new List<Action>());
+			}
+			leaveCallbacks[appState].Add(callback);
+		}
+
+		public void ChangeState(AppState newState)
+		{
+			if (CurrentAppState != newState)
+			{
+				if (leaveCallbacks.TryGetValue(CurrentAppState, out List<Action> leaveActions))
+				{
+					foreach(Action action in leaveActions)
+					{
+						action.Invoke();
+					}
+				}
+				CurrentAppState = newState;
+				if (enterCallbacks.TryGetValue(CurrentAppState, out List<Action> enterActions))
+				{
+					foreach (Action action in enterActions)
+					{
+						action.Invoke();
+					}
+				}
+			}
+		}
 	}
 }
